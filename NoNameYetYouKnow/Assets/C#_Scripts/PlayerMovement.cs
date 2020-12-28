9670145c661b6683ad8735b0ff4163459d96d2b5
Jaishnu <73887147+Jaishnu@users.noreply.github.com>
2020-12-28 20:40:44 +0530

    Commited by - Jaishnu
    8:40 pm
    Monday, 28 December 2020 (IST)
    Time in Hebbal, Bengaluru, Karnataka

---
+++ NoNameYetYouKnow/Assets/C#_Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed = 300f;
+
+    Vector3 Velocity;
+
+    public float gravity = -9.81f;
+
+    public CharacterController PlayerMovementController;
+
+    void Update()
+    {
+
+        playerMovement();
+
+    }
+
+    //Fuction shud be added in update 
+
+    public void playerMovement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+
+        Velocity.y += gravity * Time.deltaTime;
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+
+        PlayerMovementController.Move(move * MoveSpeed * Time.deltaTime);
+        PlayerMovementController.Move(Velocity * Time.deltaTime);
+
+
+    }
+}
---
+++ NoNameYetYouKnow/Assets/C#_Scripts/PlayerRotation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    [Range(1,300)]
+    public float RotationSpeed;
+    public Transform Player, Camera;
+    public float MouseX, MouseY, XRotation;
+
+    private void Start()
+    {
+        //Cursor will be locked and will not be seen if u add this line.
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    private void Update()
+    {
+        //This line unlocks the curson when you press the ESCAPE key on our keyboard
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+
+        float mouseX = Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime;
+        float MouseY = Input.GetAxis("Mouse Y") * RotationSpeed * Time.deltaTime;
+
+        XRotation -= MouseY;
+        XRotation = Mathf.Clamp(XRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(XRotation, 0, 0);
+        Player.Rotate(Vector3.up * mouseX);
+
+
+    }
+}
